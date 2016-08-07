5bb7c7c7ae8d158db92e1be9389d23cf352a8a7e
safrin <mylfieofjava@gmail.com>
2016-08-07 16:11:46 +0530

    Signed-off-by: safrin <mylfieofjava@gmail.com>

--- AdminWeb/target/AdminWeb-1.0-SNAPSHOT/resources/js/service.js
+++
@@ -1,8 +0,0 @@
-angular
-        .module('pharmacyApp.services', []).factory('Category', function($resource) {
-    return $resource('http://localhost:8080/AdminAPI/category/:id', {id: '@id'}, {
-        update: {
-            method: 'PUT'
-        }
-    });
-});

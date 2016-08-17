722bc544580b21a836377e2f745125983107fea0
safrin <mylfieofjava@gmail.com>
2016-08-17 20:18:40 +0530

    Signed-off-by: safrin <mylfieofjava@gmail.com>

---
+++ AdminAPI/src/main/java/com/config/AppConfig.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.config;
+
+import java.util.Properties;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.HibernateTemplate;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ *
+ * @author ACM Safrin
+ */
+@Configuration
+@EnableTransactionManagement
+@ComponentScan({"com.service","com.db"})
+public class AppConfig {
+        @Bean
+        public SessionFactory sessionFactory() {
+                LocalSessionFactoryBuilder builder =
+			new LocalSessionFactoryBuilder(dataSource());
+                builder.scanPackages("com.db.entity")
+                      .addProperties(getHibernateProperties());
+
+                return builder.buildSessionFactory();
+        }
+
+	private Properties getHibernateProperties() {
+                Properties prop = new Properties();
+                //prop.put("hibernate.format_sql", "true");
+                prop.put("hibernate.show_sql", "true");
+                prop.put("hibernate.dialect",
+                    "org.hibernate.dialect.MySQLDialect");
+                //prop.put("hibernate.hbm2ddl.auto","update");
+                //create-drop,create,update,validate
+                return prop;
+        }
+
+	@Bean(name = "dataSource")
+	public DriverManagerDataSource dataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+	        ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/adminapi");
+		ds.setUsername("root");
+		return ds;
+	}
+
+	//Create a transaction manager
+	@Bean
+        public HibernateTransactionManager transactionManager() {
+                return new HibernateTransactionManager(sessionFactory());
+        }
+
+        @Bean
+        public HibernateTemplate hibernateTemplate(SessionFactory sessionFactory){
+            HibernateTemplate hibernateTemplate=new HibernateTemplate();
+            hibernateTemplate.setSessionFactory(sessionFactory);
+            return hibernateTemplate;
+        }
+	
+
+}
---
+++ AdminAPI/src/main/java/com/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.config;
+
+import com.db.dao.WebUserFacade;
+import com.db.entity.WebUser;
+import com.service.WebUserService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+/**
+ *
+ * @author ACM Safrin
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    WebUserFacade webUserFacade;
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {                        
+        List<WebUser> list=webUserFacade.findAll();
+        InMemoryUserDetailsManagerConfigurer imudmc= auth.inMemoryAuthentication();
+        for(WebUser w:list){
+            imudmc.withUser(w.getName()).password(w.getWebUserPassword()).roles("ADMIN");
+        }
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        
+        http.authorizeRequests()
+		.antMatchers("/*").access("hasRole('ROLE_ADMIN')")
+                //.antMatchers("/signup", "/about").permitAll() // 
+	//	.antMatchers("/dba/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_DBA')")
+		.and().httpBasic();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/**"); // #3
+    }
+
+}
